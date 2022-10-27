13aec60bfc14574f188d8bbe1338ae401c4096fa
Nguyễn Danh Nam <izero.cs@gmail.com>
2022-10-28 00:47:17 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 import cloudscraper from "cloudscraper"
 import XRegExp from "xregexp"
 import fs from "fs"
 import path from "path"
+
+interface Comic {
+    seo: string
+    writer: string
+    inker: string
+    title: string
+    thumb: string
+    genre: Array<string>
+    chapters: Array<ChapterEntry>
+}
 
 interface ChapterEntry {
     chap: number
@@ -26,7 +36,23 @@ const http = axios.create({
 
 let arrayChapter = new Array<ChapterEntry>()
 
-const listChapterRequest = (url: string): Promise<Array<ChapterEntry>> => {
+const urlScheme = (url: string): string => {
+    if (!url.startsWith("http")) {
+        if (url.startsWith("//"))
+            return "http:" + url
+        else
+            return "http://" + url
+    }
+
+    return url
+}
+
+const urlFilename = (url: string): string => {
+    const filename = url.split('/')?.pop()?.split('#')[0].split('?')[0]
+    return (typeof filename === "undefined" ? "" : filename)
+}
+
+const listChapterRequest = (url: string): Promise<Comic> => {
     return new Promise(async (resolve, reject) => {
         cloudscraper({
             method: "GET",
@@ -35,23 +61,42 @@ const listChapterRequest = (url: string): Promise<Array<ChapterEntry>> => {
             source = source.replace(/[\r\n]+/g," ")
                            .replace("'", "\"")
 
-            const listRegex = /<div class="list-chapter" id="nt_listchapter">.+?<nav>.*?<ul>(.+?)<\/ul>/g
-            const entryRegex = /<li class="row.+?<a href="(.+?)" data-id="\d+">Chapter\s+([0-9.]+):?\s*-?\s*(.*?)<\/a>.+?<\/li>/g
-            const [ , list ] = listRegex.exec(source) || ["", ""]
-            const chapters = new Array<ChapterEntry>();
+            const titleRegex  = /<h1 class="title-detail">(.+?)<\/h1>/g
+            const thumbRegex  = /<div class="detail-info">.+?<div class="col-xs-4 col-image">.+?src="(.+?)".+?<\/div>/
+            const infoRegex   = /<div class="detail-info">.+?<ul class="list-info">(.+?)<\/ul>/g
+            const listRegex   = /<div class="list-chapter" id="nt_listchapter">.+?<nav>.*?<ul>(.+?)<\/ul>/g
+            const entryRegex  = /<li class="row.+?<a href="(.+?)" data-id="\d+">Chapter\s+([0-9.]+):?\s*-?\s*(.*?)<\/a>.+?<\/li>/g
+            const [ , title ] = titleRegex.exec(source) || ["", ""]
+            const [ , thumb ] = thumbRegex.exec(source) || ["", ""]
+            const [ , info ]  = infoRegex.exec(source)  || ["", ""]
+            const [ , list ]  = listRegex.exec(source)  || ["", ""]
+            const chapters    = new Array<ChapterEntry>()
+            const genre       = new Array<string>()
+
+            const [ , author ] = /<li class="author.+?<p class="col-xs-8">(.+?)<\/p>/g.exec(info) || ["", ""]
+            const [ , status ] = /<li class="status.+?<p class="col-xs-8">(.+?)<\/p>/g.exec(info) || ["", ""]
+            const [ , kind ]   = /<li class="kind.+?<p class="col-xs-8">(.+?)<\/p>/g.exec(info)   || ["", ""]
+            const [ , seo ]    = /\/([a-zA-Z0-9\-\_\.]+)(-\d*)$/gi.exec(url)                      || ["", ""]
+
+            XRegExp.forEach(kind, /<a href=.+?>(.+?)<\/a>/g, ([ , value ]) =>
+                genre.push(value))
 
             XRegExp.forEach(list, entryRegex, ([ , uri, chap, label ]) =>
-                chapters.push({ chap: Number.parseInt(chap), uri: uri, label: label }))
+                chapters.push({ chap: Number.parseFloat(chap), uri: uri, label: label }))
 
-            if (typeof chapters.length === "undefined" || chapters.length <= 0)
+            if (typeof chapters.length === "undefined" || chapters.length <= 0) {
                 reject(new Error("Not found list chapter in url " + url))
-            else
-                resolve(chapters)
+            } else {
+                resolve({
+                    title: title, thumb: urlScheme(thumb), writer: author,
+                    inker: author, genre: genre, chapters: chapters, seo: seo
+                })
+            }
         }).catch((err: Error) => console.error(err))
     })
 }
 
-const listImageRequest = (chapter: ChapterEntry): Promise<Array<ImageEntry>> => {
+const listImageRequest = (comic: Comic, chapter: ChapterEntry): Promise<Array<ImageEntry>> => {
     return new Promise(async (resolve, reject) => {
         cloudscraper({
             method: "GET",
@@ -63,31 +108,93 @@ const listImageRequest = (chapter: ChapterEntry): Promise<Array<ImageEntry>> => 
             const listRegex = /<div class="reading-detail box_doc">(.+?)<div class="container">/g
             const imgRegex = /<div id="page.+?<img.+?data-original="(.+?)"\s+data-cdn="(.+?)"/g
             const [, list] = listRegex.exec(source) || [ "", "" ]
-            const images = new Array<ImageEntry>();
+            const images = new Array<ImageEntry>()
 
             XRegExp.forEach(list, imgRegex, ([ , original, cdn ], index) =>
-                images.push({ index: index, original: original, cdn: cdn }))
+                images.push({ index: index, original: urlScheme(original), cdn: urlScheme(cdn) }))
 
             if (typeof images.length === "undefined" || images.length <= 0)
                 reject(new Error("Not found list image in chapter: " + chapter.uri))
+            else if (!storageMaker(comic, chapter))
+                reject(new Error("Storage maker chap failed: " + chapter.chap))
             else
                 resolve(images)
         }).catch((err: Error) => console.error(err))
     })
 }
 
-// listChapterRequest("https://www.nettruyenme.com/truyen-tranh/toi-da-chuyen-sinh-thanh-slime-100620")
-listChapterRequest("https://www.nettruyenme.com/truyen-tranh/vi-so-dau-nen-em-tang-max-vit-193640")
-    .then(chapters => {
-        console.log("Chapter request list success")
-        listImageRequest(chapters[0])
-            .then(images => {
-                const url = "https:" + images[0].original
-                console.log("Download image: " + url)
-                http.get(url, { responseType: "stream" }).then(res => {
-                    res.data.pipe(fs.createWriteStream("./001.jpg"))
-                            .on("error", console.error)
-                            .once("close", () => console.log("Successful download image"))
-                }).catch(err => console.error(err))
+const chapName = (chapter: ChapterEntry): string => {
+    const prefix = "Chapter "
+    const labelSub = " - "
+
+    if (chapter.label.length <= 0)
+        return prefix + chapter.chap
+
+    return prefix + chapter.chap + labelSub + chapter.label
+}
+
+const storagePath = (comic: Comic, chapter: ChapterEntry): string =>
+    path.join(path.dirname(__dirname), "storage", comic.seo, chapName(chapter))
+
+const storageMaker = (comic: Comic, chapter: ChapterEntry): boolean => {
+    const chapPath = storagePath(comic, chapter)
+
+    if (!fs.existsSync(chapPath) && typeof fs.mkdirSync(chapPath, { recursive: true }) === "undefined")
+        return false
+
+    return true
+}
+
+const downloadImage = (comic: Comic, chapter: ChapterEntry, image: ImageEntry): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        http.get(image.original, { responseType: "stream" })
+            .then(async res => resolve(await writeImage(comic, chapter, image.original, image, res)))
+            .catch(err => {
+                if (err.response && err.response.status != 200) {
+                    http.get(image.cdn, { responseType: "stream" })
+                        .then(async res => resolve(await writeImage(comic, chapter, image.cdn, image, res)))
+                        .catch(err => reject(err))
+                } else {
+                    reject(err)
+                }
             })
+    })
+}
+
+const writeImage = (comic: Comic, chapter: ChapterEntry, url: string, image: ImageEntry, res: AxiosResponse): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        const nameRegex = /(\d+)[a-zA-Z0-9-_]*(.jpg|.jpeg|.png|.bmp|.webp)(\?.+?)*/gi
+        const [ , num, format ] = nameRegex.exec(urlFilename(url)) || ["", "", ""]
+        const filepath = path.join(storagePath(comic, chapter), num + format)
+
+        res.data.pipe(fs.createWriteStream(filepath))
+                .on("error", () => reject(new Error("Write image failed: " + filepath)))
+                .once("close", () => resolve(true))
+    })
+}
+
+const url = "https://www.nettruyenin.com/truyen-tranh/vi-so-dau-nen-em-tang-max-vit-19364"
+// const url = "https://www.nettruyenme.com/truyen-tranh/toi-da-chuyen-sinh-thanh-slime-100620"
+
+listChapterRequest(url)
+    .then(async comic => {
+        console.log("Mange detail =>", {
+            Title: comic.title,
+            Author: comic.writer,
+            SEO: comic.seo,
+            Genre: comic.genre
+        })
+
+        let chapter
+        for (let i = 0; i < comic.chapters.length; ++i) {
+            chapter = comic.chapters[i]
+
+            console.log("Request list image chap:", chapter.chap)
+            const images = await listImageRequest(comic, chapter)
+
+            for (let k = 0; k < images.length; ++k) {
+                console.log("Chap", chapter.chap, "download image:", images[k].original)
+                await downloadImage(comic, chapter, images[k])
+            }
+        }
     }).catch(err => console.log(err))
