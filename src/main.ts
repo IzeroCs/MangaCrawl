31007cba2fe764a79459b46a266043a65ec21c16
Nguyễn Danh Nam <izero.cs@gmail.com>
2022-10-31 00:53:45 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import axios, { AxiosResponse } from "axios"
 import cloudscraper from "cloudscraper"
 import XRegExp from "xregexp"
+import xmlbuilder from "xmlbuilder"
 import fs from "fs"
 import path from "path"
 
+const AUTHOR_STATUS_UPDATING = "Đang cập nhật"
+const COMIC_STATUS_COMPLETED = "Hoàn thành"
+const COMIC_STATUS_ONGOING = "Đang tiến hành"
+
 interface Comic {
     seo: string
+    author: string
     writer: string
+    penciller: string
     inker: string
     title: string
+    description: string
     thumb: string
+    status: number
     genre: Array<string>
     chapters: Array<ChapterEntry>
 }
@@ -76,6 +85,7 @@ const listChapterRequest = (url: string): Promise<Comic> => {
             const [ , author ] = /<li class="author.+?<p class="col-xs-8">(.+?)<\/p>/g.exec(info) || ["", ""]
             const [ , status ] = /<li class="status.+?<p class="col-xs-8">(.+?)<\/p>/g.exec(info) || ["", ""]
             const [ , kind ]   = /<li class="kind.+?<p class="col-xs-8">(.+?)<\/p>/g.exec(info)   || ["", ""]
+            const [ , desc ]   = /<div class="detail-content"><p.+?>(.+?)<\/p>/g.exec(source)     || ["", ""]
             const [ , seo ]    = /\/([a-zA-Z0-9\-\_\.]+)(-\d*)$/gi.exec(url)                      || ["", ""]
 
             XRegExp.forEach(kind, /<a href=.+?>(.+?)<\/a>/g, ([ , value ]) =>
@@ -87,9 +97,20 @@ const listChapterRequest = (url: string): Promise<Comic> => {
             if (typeof chapters.length === "undefined" || chapters.length <= 0) {
                 reject(new Error("Not found list chapter in url " + url))
             } else {
+                let authorValue = author
+                let statusValue = 0
+
+                if (author == AUTHOR_STATUS_UPDATING)
+                    authorValue = ""
+
+                if (status == COMIC_STATUS_ONGOING)
+                    statusValue = 1
+                else if (status == COMIC_STATUS_COMPLETED)
+                    statusValue = 2
+
                 resolve({
-                    title: title, thumb: urlScheme(thumb), writer: author,
-                    inker: author, genre: genre, chapters: chapters, seo: seo
+                    title: title, thumb: urlScheme(thumb), author: authorValue, writer: author, penciller: author,
+                    inker: author, genre: genre, chapters: chapters, seo: seo, status: statusValue, description: desc
                 })
             }
         }).catch((err: Error) => console.error(err))
@@ -115,7 +136,7 @@ const listImageRequest = (comic: Comic, chapter: ChapterEntry): Promise<Array<Im
 
             if (typeof images.length === "undefined" || images.length <= 0)
                 reject(new Error("Not found list image in chapter: " + chapter.uri))
-            else if (!storageMaker(comic, chapter))
+            else if (!storageChapMaker(comic, chapter))
                 reject(new Error("Storage maker chap failed: " + chapter.chap))
             else
                 resolve(images)
@@ -133,11 +154,23 @@ const chapName = (chapter: ChapterEntry): string => {
     return prefix + chapter.chap + labelSub + chapter.label
 }
 
-const storagePath = (comic: Comic, chapter: ChapterEntry): string =>
-    path.join(path.dirname(__dirname), "storage", comic.seo, chapName(chapter))
+const storagePath = (comic: Comic): string =>
+    path.join(path.dirname(__dirname), "storage", comic.seo)
 
-const storageMaker = (comic: Comic, chapter: ChapterEntry): boolean => {
-    const chapPath = storagePath(comic, chapter)
+const storageChapPath = (comic: Comic, chapter: ChapterEntry): string =>
+    path.join(storagePath(comic), chapName(chapter))
+
+const storageMaker = (comic: Comic): boolean => {
+    const stPath = storagePath(comic)
+
+    if (!fs.existsSync(stPath) && typeof fs.mkdirSync(stPath, { recursive: true }) === "undefined")
+        return false
+
+    return true
+}
+
+const storageChapMaker = (comic: Comic, chapter: ChapterEntry): boolean => {
+    const chapPath = storageChapPath(comic, chapter)
 
     if (!fs.existsSync(chapPath) && typeof fs.mkdirSync(chapPath, { recursive: true }) === "undefined")
         return false
@@ -165,11 +198,79 @@ const writeImage = (comic: Comic, chapter: ChapterEntry, url: string, image: Ima
     return new Promise((resolve, reject) => {
         const nameRegex = /(\d+)[a-zA-Z0-9-_]*(.jpg|.jpeg|.png|.bmp|.webp)(\?.+?)*/gi
         const [ , num, format ] = nameRegex.exec(urlFilename(url)) || ["", "", ""]
-        const filepath = path.join(storagePath(comic, chapter), num + format)
+        const filepath = path.join(storageChapPath(comic, chapter), num + format)
 
         res.data.pipe(fs.createWriteStream(filepath))
                 .on("error", () => reject(new Error("Write image failed: " + filepath)))
                 .once("close", () => resolve(true))
+    })
+}
+
+const writeCover = (comic: Comic): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        const filepath = path.join(storagePath(comic), "cover.jpg")
+
+        if (typeof comic.thumb === "undefined")
+            resolve(true)
+
+        http.get(comic.thumb, { responseType: "stream" })
+            .then(async res => {
+                res.data.pipe(fs.createWriteStream(filepath))
+                        .on("error", () => reject(new Error("Write thumb failed: " + filepath)))
+                        .once("close", () => resolve(true))
+            }).catch(err => {
+                reject(err)
+            })
+    })
+}
+
+const writeDetail = (comic: Comic): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        const filepath = path.join(storagePath(comic), "details.json")
+        const detail = {
+            title: comic.title,
+            author: comic.author,
+            artist: comic.author,
+            description: comic.description,
+            genre: comic.genre.join(", "),
+            status: comic.status
+        }
+
+        if (!storageMaker(comic))
+            reject(new Error("Storage maker failed: " + storagePath(comic)))
+
+        try {
+            fs.writeFileSync(filepath, JSON.stringify(detail, null, 2))
+            resolve(true)
+        } catch (e) {
+            reject(new Error("Write detail failed: " + filepath))
+        }
+    })
+}
+
+const writeComicInfo = (comic: Comic, chapter: ChapterEntry): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        const filepath = path.join(storageChapPath(comic, chapter), "ComicInfo.xml")
+        const xml = xmlbuilder.create("ComicInfo")
+            .ele({
+                Series: "",
+                Number: chapter.chap,
+                Writer: comic.writer,
+                Penciller: comic.penciller,
+                Inker: comic.inker,
+                Title: chapName(chapter),
+                Summary: {},
+                Genre: comic.genre.join(", ")
+            }).end({
+                pretty: true, newline: "\n", indent: "  "
+            })
+
+        try {
+            fs.writeFileSync(filepath, xml, { encoding: "utf8" })
+            resolve(true)
+        } catch (e) {
+            reject(new Error("Writer ComicInfo failed: " + filepath))
+        }
     })
 }
 
@@ -178,6 +279,9 @@ const url = "https://www.nettruyenin.com/truyen-tranh/vi-so-dau-nen-em-tang-max-
 
 listChapterRequest(url)
     .then(async comic => {
+        await writeDetail(comic)
+        await writeCover(comic)
+
         console.log("Mange detail =>", {
             Title: comic.title,
             Author: comic.writer,
@@ -191,6 +295,7 @@ listChapterRequest(url)
 
             console.log("Request list image chap:", chapter.chap)
             const images = await listImageRequest(comic, chapter)
+            await writeComicInfo(comic, chapter)
 
             for (let k = 0; k < images.length; ++k) {
                 console.log("Chap", chapter.chap, "download image:", images[k].original)
